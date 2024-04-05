955f7152d18781c07fc9c78de83be8aa615d9717
LuizSilva28 <luizssh54@gmail.com>
2024-04-05 11:02:14 -0300

    aula_33 do yt - acessando elemento atraves do Dom

--- 6 - JavaScript/aula-16/script.js
+++ 6 - JavaScript/aula-16/script.js
@@ -14,3 +14,14 @@ document.getElementById('type').addEventListener('click', function () {
 document.getElementById('placeholder').addEventListener('click', function () {
     input.placeholder = input.placeholder === ''? 'Digite seu nome': '';
 })
+
+document.getElementById('disable').addEventListener('click', function () {
+  input.setAttribute('disabled', !input.disabled)
+})
+
+document.getElementById('data').addEventListener('click', function () {
+  const data = input.dataset.somethingElse
+  console.log("O valor do atributo data-something-else é: " + data)
+  input.dataset.somethingElse = 'Algum outro valor'
+  console.log("O valor do atributo data-something-else agora é: " + input.dataset.somethingElse)
+})
