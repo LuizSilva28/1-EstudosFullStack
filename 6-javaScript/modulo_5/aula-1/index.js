ecb9adc2f2470164b70bb8b5ac0a0f9e3afdde76
Luiz Henrique <120956095+LuizSilva28@users.noreply.github.com>
2024-10-10 20:39:32 -0300

    modulo-5 aula-1

--- 6-javaScript/modulo_5/aula-1/index.js
+++ 6-javaScript/modulo_5/aula-1/index.js
@@ -1,4 +1,5 @@
-const book = {
+/*
+	const book = {
 	title: "Eragon",
 	pages: 468,
 	published: true,
@@ -11,16 +12,41 @@ const book = {
 		this.inStock += quantity;
 	},
     
-};
+	};
 
-console.log(book);
-
-book.addOnStock(50);
-
-
-
-book.save = function() {
+	book.save = function() {
         // salva no banco de dados
     }
-    
-console.log(book);
+
+	console.log(book);
+*/
+
+
+
+
+
+function Book (title, pages, tags, author) {
+	this.title = title;
+	this.pages = pages;
+	this.tags = tags;
+	this.author = author;
+	this.published = false;
+	this.inStock = 0;
+	this.addOnStock = function (quantity) {
+		this.inStock += quantity;
+	};
+	this.save = function () {
+		//salva no banco de dados
+	};
+}
+
+const author = { name: "Christopher Paulini"}
+const tags = ["fantasy", "adventure", "medieval"];
+
+
+const eragon = new Book("Eragon", 469, tags, author);
+console.log(eragon);
+
+const eldest = new Book("Eldest", 644, tags, author);
+
+console.log(eldest);
