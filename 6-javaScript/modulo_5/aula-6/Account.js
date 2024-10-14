ca45e01aebc303743db279d398b9fd719b0eefd9
Luiz Henrique <120956095+LuizSilva28@users.noreply.github.com>
2024-10-14 20:14:38 -0300

    up aula-6

---
+++ 6-javaScript/modulo_5/aula-6/Account.js
@@ -0,0 +1,31 @@
+class Account {
+	#password;
+	#balance = 0;
+	constructor(user) {
+		this.email = user.email;
+		this.#password = user.password;
+		
+	}
+    getBalance(email, password){
+        if(this.#authenticate(email, password)){
+            return this.#balance
+        } else{
+            return null
+        }
+    }
+
+    #authenticate(email, password){
+        return this.email === email && this.#password === password;
+    }
+}
+
+const user = {
+	email: "john.doe@example.com",
+	password: "password123",
+};
+
+const myAccount = new Account(user);
+
+console.log(myAccount);
+
+console.log(myAccount.getBalance("john.doe@example.com", "password123"));
