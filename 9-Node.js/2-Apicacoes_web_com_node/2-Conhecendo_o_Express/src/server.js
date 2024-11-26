ef6791b2fb14aa10855e83beafe8f536aa25ce96
Luiz Henrique <120956095+LuizSilva28@users.noreply.github.com>
2024-11-26 12:54:48 -0300

    up node aula-2

---
+++ 9-Node.js/2-Apicacoes_web_com_node/2-Conhecendo_o_Express/src/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (request, response) => {
+	response.send(
+		"Servidor Express funcionando \nVocê está na página inicial!"
+	);
+});
+
+// server.get("/artigos", (request, response) => {
+// 	response.send(
+// 		"Servidor Express funcionando \nVocê está na página de artigos!"
+// 	);
+// });
+server.post("/artigos", (req, res) => {
+	res.send(
+		"Você está na página de artigos!"
+	);
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+	console.log(`Servidor Express iniciado em http://localhost:${PORT}/`);
+});
