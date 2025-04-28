fbca5b2139f526b6b7a2ff01a621f714e2f9560d
Luiz <luizssh54@gmail.com>
2025-04-28 08:29:11 -0300

    add aula 5-enums

---
+++ 12-Typescript/5-enums/enums.ts
@@ -0,0 +1,20 @@
+enum Planets {
+	MERCURIO,
+	VENUS,
+	TERRRA,
+	MARTE,
+}
+
+enum PlanetsOp2 {
+	MERCURIO = 1,
+	VENUS,
+	TERRRA,
+	MARTE,
+}
+
+enum Database {
+	CREATE = "create",
+	DELETE = "delete",
+	UPADET = "upadt",
+	READ = " read",
+}
