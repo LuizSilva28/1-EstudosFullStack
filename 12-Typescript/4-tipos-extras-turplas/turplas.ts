8d9dcfcfeceda33f870cbe800380603727bfa676
Luiz <luizssh54@gmail.com>
2025-04-28 08:27:51 -0300

    add aulas 4-tipos-extras-turplas

---
+++ 12-Typescript/4-tipos-extras-turplas/turplas.ts
@@ -0,0 +1,16 @@
+let crew: string[] = [];
+
+crew.push("Luiz");
+
+let crew2: [string, string, string] = ['', '',''];
+crew2[0] = "Luiz";
+crew2[1] = "Henrique";
+crew2[2] = "Santos";
+
+let crew3: [number, string, boolean];
+crew3 = [1, "Henrique", true];
+
+let point: [number, number]
+point = [2, 5]
+
+let [x, y] = point
