73542200d2251c48e6e9939d1b808398b7638860
Luiz Silva <luizssh54@gmail.com>
2025-06-11 07:22:09 -0300

    adicionando as aulas de react do curso

---
+++ 7-React/aula-7_execicio_4/exercicio-4_gerador_de_senhas/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='app'>
+      <h1>Gerador de senhas</h1>
+      <button>Gerar!</button>
+      <button>Copiar</button>
+      <div>senha</div>
+    </div>
+  )
+}
+
+export default App
